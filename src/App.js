a43ed4e64edd42d23bfac5f57b0b23dcfde61748
Nathanael White <narhanaelw99@gmail.com>
2021-07-29 17:54:24 -0500

    added conditional rendering to the nav and tested everything for contact

--- src/App.js
+++ src/App.js
@@ -14,8 +14,9 @@ function App() {
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
-
+  
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
@@ -23,11 +24,19 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <ContactFrom></ContactFrom>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactFrom></ContactFrom>
+        )}
+        
       </main>
     </div>
   );
